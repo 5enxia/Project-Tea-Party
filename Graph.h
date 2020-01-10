083c354bae32e307724a742e93648b083b55812b
takayasu_morishita <c011729632@edu.teu.ac.jp>
2020-01-10 14:29:01 +0900

    delete {Graph, Viewer, data}.h

--- Graph.h
+++
@@ -1,7 +0,0 @@
-namespace Graph{
-	int map(float val, int min, int max, float new_min, float new_max) {
-		float div = (float)(max - min);
-		float new_div = new_max - new_min;
-		return (int)(val*(new_div / div));
-	}
-}
